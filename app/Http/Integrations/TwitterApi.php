e9d2d44b810c1745f6c72d7ef043fef5df01531f
Ilya Chyzh <ichyzh@student.unit.ua>
2020-03-05 13:54:42 +0200

    edit: add keys for TwitterAPI

--- app/Http/Integrations/TwitterApi.php
+++ app/Http/Integrations/TwitterApi.php
@@ -6,6 +6,11 @@ use App\User;
 
 class TwitterApi
 {
+    private $access_token = '1235177379980627970-ppbo1aLvIO2ObCfv5WNJ9Wpv7XQ93e';
+    private $acces_token_secret = 'AQok59obBgGRaTpn5F1e9ZdIyh9smwDQ8SWDzaL7qlAKo';
+    private $consumer_key = 'kxVDVFbNDDG9Ef9Tbe6dGeQ8B';
+    private $consumer_secret = 'xpvpi9Qy62rgFqfgzTRfgnbENHhDgyB8aXtWDX36C8DJVqho2f';
+
     /**
      * Number of posts from user
      */
@@ -13,8 +18,8 @@ class TwitterApi
 
     public function getTweets(string $name) :array
     {
-        $twitter = new TwitterOAuth(env('TWITTER_CONSUMER_KEY'), env('TWITTER_CONSUMER_SECRET'), env('TWITTER_ACCESS_TOKEN'), env('TWITTER_ACCESS_TOKEN_KEY'));
-    
+        $twitter = new TwitterOAuth($this->consumer_key, $this->consumer_secret, $this->access_token, $this->acces_token_secret);
+        
         $response = [];
 
         $tweets = $twitter->get("statuses/user_timeline", ["count" => $this->count, 'trim_user' => true, "exclude_replies" => true, 'screen_name' => $name]);
